38e3a5e8ed0dcf322a5985b63d796ffb79a3f048
Samara1404 <stambekova1404@gmail.com>
2023-11-07 23:44:09 +0600

    if_else

---
+++ bin/if_else.dart
@@ -0,0 +1,10 @@
+
+
+void main(List<String> arguments) {
+ int number = 35;
+  if (number > 5) {
+    print("Number is greater than 5");
+  } else {
+    print("Number is less than 5");
+ }
+}
---
+++ test/if_else_test.dart
@@ -0,0 +1,8 @@
+import 'package:if_else/if_else.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
